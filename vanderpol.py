3ef9509adbcf06276b0163973b25c80e995504a0
Sri Harsha Gangisetty <130010051@iitb.ac.in>
2016-10-18 23:31:36 +0530

    added codee to solve vanderpol

--- vanderpol.py
+++ vanderpol.py
@@ -2,8 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 
-def vanderpol_func(dim,t): 
-	x, y = dim
+
+def vanderpol_func(X,t): 
+	x, y = tuple(X)
 	dxdt = y
 	dydt = mu*(1.0-x*x)*y-x
 	return [dxdt,dydt]
+
+x0 = [1,2]
+t = np.linspace(0,100,1000)
+
+solution = odeint(vanderpol_func,x0,t)
