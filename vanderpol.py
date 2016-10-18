11d3a0bdeb414d70fd696b152f3516e176ed79d1
Sri Harsha Gangisetty <130010051@iitb.ac.in>
2016-10-18 23:39:41 +0530

    added plotting functions

--- vanderpol.py
+++ vanderpol.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 
-
+mu = 1
 def vanderpol_func(X,t): 
 	x, y = tuple(X)
 	dxdt = y
@@ -13,3 +13,10 @@ x0 = [1,2]
 t = np.linspace(0,100,1000)
 
 solution = odeint(vanderpol_func,x0,t)
+
+#adding plotting functions
+x = [ele[0] for ele in solution]
+y = [ele[1] for ele in solution]
+plt.plot(t,x)
+plt.plot(t,y)
+plt.show()
